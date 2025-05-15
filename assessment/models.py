b4a77f6d048cc528d2cf467be3562dde96736889
Justine <mbilinyijustin14@gmail.com>
2025-05-15 18:31:12 +0300

    Adding assessment class

--- assessment/models.py
+++ assessment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class SoilAssessment(models.Model):
     farmer_name = models.CharField(max_length=100)
@@ -12,3 +13,13 @@ class SoilAssessment(models.Model):
     def __str__(self):
         return f"{self.farmer_name} - {self.submission_date.strftime('%Y-%m-%d')}"
 
+
+class Assessment(models.Model):
+    soil_type = models.CharField(max_length=100)
+    moisture_level = models.DecimalField(max_digits=5, decimal_places=2)
+    fertility = models.CharField(max_length=100)
+    farm_size = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.soil_type} - {self.moisture_level} - {self.farm_size}"
