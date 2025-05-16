4e7d961d27873e287daba59a988d26b1dbca14fc
Justine <mbilinyijustin14@gmail.com>
2025-05-16 17:55:39 +0300

    updating the urls

--- assessment/urls.py
+++ assessment/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name= 'home'),
-    path('', views.submit_soil_assessment, name='submit_soil_assessment'),
-    path('success/', views.success, name='success'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
