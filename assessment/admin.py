cdb2e8beedccd4d530b8c76b4578d6bdfde52aae
Justine <mbilinyijustin14@gmail.com>
2025-05-16 19:29:00 +0300

    adding soilassessment in admin

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
 from .models import Assessment
+from django.contrib import admin
+from .models import SoilAssessment
+
+@admin.register(SoilAssessment)
+class SoilAssessmentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'ph', 'moisture', 'fertility', 'farm_size', 'weather', 'created_at']
+    list_filter = ['weather', 'created_at']
+    search_fields = ['user__username', 'fertility']
 
 # Register your models here.
 admin.site.register(Assessment)
