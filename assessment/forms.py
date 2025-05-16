35da3c5d636c9569c9711873af0ca4875755b207
Justine <mbilinyijustin14@gmail.com>
2025-05-16 19:32:19 +0300

    adding soilassessment in forms

--- assessment/forms.py
+++ assessment/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import SoilAssessment
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import SoilAssessmentForm
 class SoilAssessmentForm(forms.ModelForm):
     class Meta:
         model = SoilAssessment
@@ -12,3 +14,16 @@ class SoilAssessmentForm(forms.ModelForm):
             'soil_ph': forms.NumberInput(attrs={'class': 'form-control'}),
             'soil_temperature': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+@login_required
+def new_soil_assessment(request):
+    if request.method == 'POST':
+        form = SoilAssessmentForm(request.POST)
+        if form.is_valid():
+            assessment = form.save(commit=False)
+            assessment.user = request.user
+            assessment.save()
+            return redirect('assessment_result', pk=assessment.pk)
+    else:
+        form = SoilAssessmentForm()
+    return render(request, 'assessment/new_assessment.html', {'form': form})
