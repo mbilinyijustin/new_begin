e467e7688063d21c5dad8f3871fcca83627772db
Justine <mbilinyijustin14@gmail.com>
2025-05-17 10:27:42 +0300

    updating urls

--- assessment/urls.py
+++ assessment/urls.py
@@ -6,8 +6,8 @@ app_name = 'assessment'
 urlpatterns = [
     #path('create/', views.create_assessment, name='create'),
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('new-assessment/', views.new_assessment, name='new_assessment'),
